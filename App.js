364d3c53ae5c5074e67cabda26233a06facddd1b
Alexandra Antal <antal.alexandra98@gmail.com>
2019-10-18 14:00:01 +0300

    Map user location

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+//import { Google } from 'expo';
+
+import FetchLocation from './components/FetchLocation';
+import UsersMap from './components/UsersMap';
+
+export default class App extends React.Component {
+
+  state = {
+    userLocation: null
+  }
+
+  // const GET_LOCATION_OPTIONS = {
+  //   enableHighAccuracy: false,
+  //   timeout: 20000,
+  //   maximumAge: 1000,
+  //  };
+
+  getUserLocationHandler = () => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+        this.setState ({
+          userLocation: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0622,
+            longitudeDelta: 0.0421
+          }
+        });
+      }, (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 });
+  };
+
+  render(){
+  return (
+    <View style={styles.container}>
+      <FetchLocation onGetLocation={this.getUserLocationHandler}/>
+    <UsersMap userLocation={this.state.userLocation}/>
+    </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
